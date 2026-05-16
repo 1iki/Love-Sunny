9892181e84e118d874ffee247a93ea3e3808702f
1iki <87522077+1iki@users.noreply.github.com>
2026-05-17 03:15:25 +0700

    ci: add mongodb service and copy static assets for standalone playwright testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
 
   // Bagian terpenting: Menjalankan server Next.js otomatis sebelum testing
   webServer: {
-    command: 'npm run build && PORT=3000 node .next/standalone/server.js',
+    command: 'npm run build && cp -r public .next/standalone/ && cp -r .next/static .next/standalone/.next/ && PORT=3000 node .next/standalone/server.js',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000, // Memberikan waktu 2 menit untuk build
